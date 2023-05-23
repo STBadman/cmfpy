5f10a0852d6b15f8ed3be9a86a4055fdaba69072
Samuel Badman <samuel.badman@cfa.harvard.edu>
2023-05-23 15:14:00 -0400

    Add working 'make euvmap' function, and example in topLevel.ipynb

--- CHmetric/CHmetric.py
+++ CHmetric/CHmetric.py
@@ -7,11 +7,18 @@ fits file
 3) <DO CH SCORE> Read in an "observed CH" map and "modeled CH" map, 
 compute precision, recall and f-score
 '''
-
 import astropy.units as u
+import datetime
+import os
+import numpy as np
+from sunpy.coordinates.sun import L0
+from sunpy.net import Fido, attrs as a
+import sunpy.map
+import sys
 
-def create_euv_map(center_date,euv_obs_cadence=1*u.day,
-                   save_dir='./'
+def create_euv_map(center_date,
+                   euv_obs_cadence=1*u.day,
+                   save_dir='./CHmetric/data/'
                    ) :
     '''
     Given `center_date`:`datetime.datetime` download a Carrington 
@@ -22,7 +29,76 @@ def create_euv_map(center_date,euv_obs_cadence=1*u.day,
     carrington longitude gaussian weighting to obtain a full sun EUV
     map. Save as fits file in `save_dir`:`str`
     '''
-    pass
+
+    ## First, check if map centered on center_date has already been created 
+    ## if it has, jump straight to the end.
+    savepath = f"{save_dir}/{center_date.strftime('%Y-%m-%d')}.fits"
+    if not os.path.exists(savepath) :
+
+        ## Use sunpy Fido to search for AIA 193 data over a carrinton rotation
+        ### If the downloads succeed once, they are archived on your computer in
+        ### ~/sunpy/data and a database entry is created. This step then becomes 
+        ### much quicker on the next run.
+        sys.stdout.write(f"Searching for input EUV maps")
+        res=Fido.search(
+            a.Time(center_date-datetime.timedelta(days=14), 
+                center_date+datetime.timedelta(days=14)
+            ), 
+            a.Instrument.aia,
+            a.Wavelength(193*u.angstrom), 
+            a.Sample(euv_obs_cadence)
+            )  
+        ## Download, or return files if already exist
+        downloaded_files = Fido.fetch(res)
+
+        ## Read in downloaded data as `sunpy.map.Map` objects and downsample
+        downsample_dims = [1024,1024] * u.pixel
+        carrington_rotation = [sunpy.map.Map(m).resample(downsample_dims) 
+                            for m in downloaded_files]
+        
+        ## Loop through input maps and reproject each one to the Carrington frame
+        shape_out = (360, 720)
+        carrington_maps = []
+        sys.stdout.write(f"Reprojecting {len(carrington_rotation)} Maps: \n")
+        for ii,m in enumerate(carrington_rotation) :
+            sys.stdout.write(f"{ii+1:02d}/{len(carrington_rotation)}\r")
+            header =  sunpy.map.make_heliographic_header(m.date, m.observer_coordinate, 
+                                            shape_out, frame='carrington')
+            carrington_maps.append(m.reproject_to(header))
+        
+        ## Combine maps together with gaussian weighting
+
+        ### Make header for combined map (use central map as reference)
+        ref_map = carrington_rotation[14]
+        ref_date = ref_map.meta['date-obs']
+        ref_coord = ref_map.observer_coordinate
+        ref_header = sunpy.map.header_helper.make_heliographic_header(
+            ref_map.date, ref_coord, shape_out, frame="carrington"
+        )
+
+        ### Compute a gaussian weight for each pixel in each map.
+        gaussian_width_lon = 60
+        gaussian_weights = [
+        np.exp(-((sunpy.map.all_coordinates_from_map(m).lon.to("deg").value 
+                -L0(m.date).to("deg").value + 180) % 360 - 180)**2
+            /(2*gaussian_width_lon**2)
+            ) 
+        for m in carrington_maps
+        ]
+
+        ### Average all maps together
+        combined_map_gaussian_weights = sunpy.map.Map(
+            np.nanmean([
+                m.data*w for m,w in 
+                zip(carrington_maps,gaussian_weights)
+                ],
+                axis=0),ref_header)
+
+        ## Save output combined map as fits file
+        combined_map_gaussian_weights.save(savepath)  
+
+    ## Return output map filename
+    return savepath
 
 def extract_obs_ch(euv_map,save_dir='./') :
     '''
