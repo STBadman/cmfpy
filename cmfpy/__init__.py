d06ad7cb7eb931b21d9b7da45da7ee6d6207e14d
Andie <akisare@outlook.com>
2023-09-29 21:50:50 -0500

    fixed typo in cache_magneto_urls

--- cmfpy/__init__.py
+++ cmfpy/__init__.py
@@ -1,4 +1,3 @@
-# %%
 from cmfpy.model import model
 from cmfpy.utils import listhtml
 from numpy import save
@@ -10,10 +9,9 @@ __all__ = ['model']
 __model_cachepath__ = f'{__path__[0]}/model/__cache__'
 if not os.path.exists(__model_cachepath__): os.makedirs(__model_cachepath__)
 
-def cache_magneo_urls():
+def cache_magneto_urls():
     for year in GONG_YEARS:
         url = f'https://gong.nso.edu/adapt/maps/gong/{year}/'
         files = listhtml(url, contains=f'{year}', include_url=False)
         save(f'{__model_cachepath__}/magneto_urls_{year}.npy',files,allow_pickle=True)
-#cache_magneo_urls()
-# %%
+#cache_magneto_urls()
